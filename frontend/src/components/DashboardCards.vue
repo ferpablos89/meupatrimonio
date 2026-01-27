49f8f3f30095dfd7500d21ffd47634394115a961
Fernando Pablos <fernando.pablos@gestao.gov.br>
2026-01-27 10:10:48 -0300

    Ajustes no frontend

--- frontend/src/components/DashboardCards.vue
+++ frontend/src/components/DashboardCards.vue
@@ -1,21 +1,50 @@
-
 <template>
-  <div class="row mt-4">
-    <div class="col-md-4" v-for="card in cards" :key="card.title">
-      <div class="card shadow-sm mb-3">
-        <div class="card-body">
-          <h6>{{ card.title }}</h6>
-          <h3>{{ card.value }}</h3>
+  
+<nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm" style="background-color: #F7F7F7 !important;">
+    <div class="container-fluid">
+        <a class="navbar-brand d-flex align-items-center ms-5 me-5" href="#">
+            <img :src="logo" alt="MeuPatrimonio" style="height: 130px;">
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuPrincipal">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="menuPrincipal">
+            <ul class="navbar-nav">
+                <li class="nav-item" style="border-right: 1px solid #ccc"><a class="nav-link" href="#">Gastos</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Categorias de Gasto</a></li>
+            </ul>
         </div>
-      </div>
     </div>
-  </div>
+</nav>
+
+<main class="content container-fluid">
+    <h1 class="h4 mb-4 ms-4">Gastos do mês</h1>
+    <button class="btn btn-outline-primary mb-3 ms-4">Novo Gasto</button>
+    <table class="table ms-4 me-6">
+        <tr>
+            <th>Categoria</th>
+            <th>Gastos</th>
+        </tr>
+        <tr>
+            <td>Alimentação</td>
+            <td>R$ 1.727,49</td>
+        </tr>
+        <tr>
+            <td>Contas</td>
+            <td>R$ 2.507,97</td>
+        </tr>
+        <tr>
+            <td>Lazer</td>
+            <td>R$ 900,40</td>
+        </tr>
+    </table>
+</main>
+
+
 </template>
 
 <script setup>
-const cards = [
-  { title: 'Gastos do mês', value: 'R$ 2.500,00' },
-  { title: 'Patrimônio total', value: 'R$ 150.000,00' },
-  { title: 'Investimentos', value: 'R$ 120.000,00' },
-]
+import logo from "@/assets/logo_mp.png"
 </script>
